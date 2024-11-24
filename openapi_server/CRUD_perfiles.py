1a2dd8470b56b7f059c2d68e2c2e3491b5b09d26
Alwaylittl <jaariasr@alumnos.unex.es>
2024-11-24 12:13:00 +0100

    SCRUM-197-CU17- Crear Perfil

--- openapi_server/CRUD_perfiles.py
+++ openapi_server/CRUD_perfiles.py
@@ -8,3 +8,12 @@ def obtener_id_perfil(db: Session, id_usuario: int) -> int:
 def obtener_perfil_usuario(db: Session, id_usuario: int) -> bool:
     perfil = db.query(Perfil).filter(Perfil.usuario_id == id_usuario).first()
     return perfil is not None
+
+def obtener_id_perfil_por_nombre(db: Session, id_usuario: int, nombre_perfil: str) -> int:
+    perfil = db.query(Perfil).filter(Perfil.usuario_id == id_usuario, Perfil.nombre_perfil == nombre_perfil).first()
+    return perfil.id
+
+def agregar_perfil_usuario(db: Session,id_usuario: int, nombre_perfil: str) -> None:
+    perfil = Perfil(usuario_id=id_usuario, nombre_perfil=nombre_perfil )
+    db.add(perfil)
+    db.commit()
